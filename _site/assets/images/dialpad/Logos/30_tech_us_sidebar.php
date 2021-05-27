fc0902b7cbbefe7f0523acc47b9c647515c04de7
Public <public1@Mac-mini.local>
2021-05-28 08:06:26 +0900

    first commit

---
+++ _site/assets/images/dialpad/Logos/30_tech_us_sidebar.php
@@ -0,0 +1,40 @@
+<aside class="col-sm-3 ml-sm-auto blog-sidebar">
+          <div class="sidebar-module">
+          <div class="list-group">
+          <a href="../Americas" class="list-group-item list-group-item-action active">
+           Semiconductors</a>
+        <a href="../Japan/desk_app.php" class="list-group-item list-group-item-action"><img src="images/dialpad/logos/icons8-canada-2.png"> Canada</a>
+        <a href="../Americas/NorthAmerica/united_states.php" class="list-group-item list-group-item-action"><img height="48" src="https://s3-ap-northeast-1.amazonaws.com/all-jp-1/img/icons/countries/icons8-united-states.png"> United States</a>
+        <a href="../Americas/NorthAmerica/mexico.php" class="list-group-item list-group-item-action"><img height="48" src="https://s3-ap-northeast-1.amazonaws.com/all-jp-1/img/icons/countries/icons8-mexico-50.png"> Mexico</a>
+        <a href="../Americas/SouthAmerica/argentina.php" class="list-group-item list-group-item-action"><img src="https://s3-ap-northeast-1.amazonaws.com/all-jp-1/img/icons/countries/icons8-argentina.png"> Argentina</a>
+        <a href="../Americas/SouthAmerica/brazil.php" class="list-group-item list-group-item-action"><img src="https://s3-ap-northeast-1.amazonaws.com/all-jp-1/img/icons/countries/icons8-brazil.png"> Brazil</a>
+        <a href="../Americas/SouthAmerica/chile.php" class="list-group-item list-group-item-action"><img src="https://s3-ap-northeast-1.amazonaws.com/all-jp-1/img/icons/countries/icons8-chile.png"> Chile</a>
+        <a href="../Americas/SouthAmerica/colombia.php" class="list-group-item list-group-item-action"><img src="https://s3-ap-northeast-1.amazonaws.com/all-jp-1/img/icons/countries/icons8-colombia-48.png"> Columbia</a>
+        <a href="../Americas/SouthAmerica/ecuador.php" class="list-group-item list-group-item-action"><img src="https://s3-ap-northeast-1.amazonaws.com/all-jp-1/img/icons/countries/icons8-ecuador.png"> Ecuador</a>
+        <a href="../Americas/SouthAmerica/peru.php" class="list-group-item list-group-item-action"><img src="https://s3-ap-northeast-1.amazonaws.com/all-jp-1/img/icons/countries/icons8-peru.png"> Peru</a>
+        <a href="../Americas/SouthAmerica/uruguay.php" class="list-group-item list-group-item-action"><img src="https://s3-ap-northeast-1.amazonaws.com/all-jp-1/img/icons/countries/icons8-uruguay.png"> Uruguay</a>
+        <a href="../Americas/SouthAmerica/venezuela.php" class="list-group-item list-group-item-action"><img src="https://s3-ap-northeast-1.amazonaws.com/all-jp-1/img/icons/countries/icons8-venezuela.png"> Venezuela</a>
+        <a href="../Europe" class="list-group-item list-group-item-action active">Europe</a>
+        <a href="#" class="list-group-item list-group-item-action">United Kingdom</a>
+        <a href="france.php" class="list-group-item list-group-item-action"><img src="https://s3-ap-northeast-1.amazonaws.com/all-jp-1/img/icons/countries/icons8-france.png"> France</a>
+        <a href="germany.php" class="list-group-item list-group-item-action"><img src="https://s3-ap-northeast-1.amazonaws.com/all-jp-1/img/icons/countries/icons8-germany.png"> Germany</a>
+        <a href="sweden.php" class="list-group-item list-group-item-action"><img src="https://s3-ap-northeast-1.amazonaws.com/all-jp-1/img/icons/countries/icons8-sweden.png "> Sweden</a>
+        <a href="../Asia" class="list-group-item list-group-item-action active">Asia</a>
+        <a href="singapore.php" class="list-group-item list-group-item-action"><img height="48" src="https://s3-ap-northeast-1.amazonaws.com/all-jp-1/img/icons/countries/icons8-singapore.png"> Singapore</a>
+        <a href="hong_kong.php" class="list-group-item list-group-item-action"><img height="48" src="https://s3-ap-northeast-1.amazonaws.com/all-jp-1/img/icons/countries/icons8-hongkong-flag.png"> Hong Kong</a>
+        <a href="../../JpJobs" class="list-group-item list-group-item-action"><img src="https://s3-ap-northeast-1.amazonaws.com/all-jp-1/img/icons/japan.png"> Japan</a>
+        <a href="australia.php" class="list-group-item list-group-item-action"><img height="48" src="https://s3-ap-northeast-1.amazonaws.com/all-jp-1/img/icons/countries/icons8-australia.png"> Australia</a>
+ <a href="cambodia.php" class="list-group-item list-group-item-action"><img height="48" src="https://s3-ap-northeast-1.amazonaws.com/all-jp-1/img/icons/countries/cambodia.png"> Cambodia</a>
+        <a href="china.php" class="list-group-item list-group-item-action"><img height="48" src="https://s3-ap-northeast-1.amazonaws.com/all-jp-1/img/icons/countries/icons8-china.png"> China</a>
+ <a href="indonesia.php" class="list-group-item list-group-item-action"><img height="48" src="https://s3-ap-northeast-1.amazonaws.com/all-jp-1/img/icons/countries/icons8-singapore.png"> Indonesia</a>
+        <a href="laos.php" class="list-group-item list-group-item-action"><img height="48" src="https://s3-ap-northeast-1.amazonaws.com/all-jp-1/img/icons/countries/icons8-laos.png"> Laos</a>
+ <a href="malaysia.php" class="list-group-item list-group-item-action"><img height="48" src="https://s3-ap-northeast-1.amazonaws.com/all-jp-1/img/icons/countries/icons8-malaysia.png"> Malaysia</a>
+        <a href="myanmar.php" class="list-group-item list-group-item-action"><img height="48" src="https://s3-ap-northeast-1.amazonaws.com/all-jp-1/img/icons/countries/icons8-myanmar.png"> Myanmar</a>
+ <a href="philippines.php" class="list-group-item list-group-item-action"><img height="48" src="https://s3-ap-northeast-1.amazonaws.com/all-jp-1/img/icons/countries/icons8-philippines.png"> Philippines</a>
+        <a href="south_korea.php" class="list-group-item list-group-item-action"><img height="48" src="https://s3-ap-northeast-1.amazonaws.com/all-jp-1/img/icons/countries/icons8-south-korea.png"> South Korea</a>
+ <a href="taiwan.php" class="list-group-item list-group-item-action"><img height="48" src="https://s3-ap-northeast-1.amazonaws.com/all-jp-1/img/icons/countries/icons8-taiwan-48.png"> Taiwan</a>
+        <a href="thailand.php" class="list-group-item list-group-item-action"><img height="48" src="https://s3-ap-northeast-1.amazonaws.com/all-jp-1/img/icons/countries/icons8-thailand.png"> Thailand</a>
+    <a href="vietnam.php" class="list-group-item list-group-item-action"><img height="48" src="https://s3-ap-northeast-1.amazonaws.com/all-jp-1/img/icons/countries/icons8-vietnam.png"> Vietnam</a>
+          </div>
+       </div>
+        </aside><!-- /.blog-sidebar -->
